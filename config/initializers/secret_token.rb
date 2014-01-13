b4af7c69297d6b1cf786191771f4bc76784b35fe
Wright Rickman <wrightrickman@gmail.com>
2014-01-13 13:17:43 -0500

    First Commit :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YelpRanker::Application.config.secret_token = '3ad0a38a89cf2f23f044da76c088abe08cc48ddcfec53406719a75ee8377cb5811902827029f547c179c42b4c97fa4a29de76b4d1545520ade26cc08376d79d8'
