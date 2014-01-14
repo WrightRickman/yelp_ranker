6d2d3f2abae5564218d2bf12bde8f645db1d7ad7
Wright Rickman <wrightrickman@gmail.com>
2014-01-14 12:11:39 -0500

    restart

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 YelpRanker::Application.routes.draw do
   
-
-
   root to: 'urls#index'
 
   get '/urls/:url_string' => 'urls#parse_url'
--- spec/features/url_feature_spec.rb
+++ spec/features/url_feature_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe 'Url' do
 	before do
 		visit root_path
-		login_as(@user)
+			login_as(@user)
+
+		describe "given a URL" do
+			before do
+				@url = 'restaurantnewyorkny'
+			end
+		end
+
+		describe "user enters a url" do
+			before do
+				fill_in 'url', :with @url
+				click_button :submit
+			end
+
+			it "displays the url" do
+				current_path.should == '/'
+				page.should have_content(@url)
+			end
+		end
 	end
-
-	# describe "given a URL" do
-	# 	before do
-	# 		@url = 'restaurantnewyorkny'
-	# 	end
-	# end
-
-	# describe "user enters a url" do
-	# 	before do
-	# 		fill_in 'url', :with => @url
-	# 		click_button :submit
-	# 	end
-
-	# 	it "displays the url" do
-	# 		current_path.should == '/'
-	# 		page.should have_content(@url)
-	# 	end
-	# end
 end
