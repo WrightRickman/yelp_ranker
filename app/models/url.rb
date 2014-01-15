c9afce6946a98f9826c83c851aede61d86c4c9c0
Wright Rickman <wrightrickman@gmail.com>
2014-01-15 13:36:38 -0500

    about to add devise

--- app/models/url.rb
+++ app/models/url.rb
@@ -11,19 +11,23 @@ class Url < ActiveRecord::Base
 
 	def self.call_api(term, location)
 
+		#Sets host
 		api_host = 'api.yelp.com'
 
+		#Keys are set in an Env. Vriable
 		consumer_key = ENV['YELP_CONSUMER_KEY']
 		consumer_secret = ENV['YELP_CONSUMER_SECRET']
 		token = ENV['YELP_TOKEN']
 		token_secret = ENV['YELP_TOKEN_SECRET']
 
+		#access OAuth
 		consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
 		access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
-		#Sample url takes top 10 bars in brooklyn
+		#Sample url takes top 10 @term in @location
 		path = "/v2/search?term=#{@term}&location=#{@location}&limit=10"
 
+		#get Json from api
 		@result = access_token.get(path).body
 
 		#Cleans Json
